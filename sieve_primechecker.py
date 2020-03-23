4fd81b939fe98a9be00888436e63af180f1cb554
Jawad1347 <60029101+Jawad1347@users.noreply.github.com>
2020-03-23 16:17:59 +0000

    Update sieve_primechecker.py

--- sieve_primechecker.py
+++ sieve_primechecker.py
@@ -2,14 +2,27 @@
 TypeError: 'set' object is not subscriptable
 """
 
+# def sieve_implimented(x):
+#     prime_cheker = set()
+#     for i in range(2, x//2):
+#         for j in range(i*i, x//2, i):
+#             prime_cheker.add(j)
+#         if x % prime_cheker[i] == 0:
+#             print("Not Prime")
+#             break
+
+# k = int(input("Big number give now "))
+# sieve_implimented(k)
+
+# second iteration with help of numpy
+
 def sieve_implimented(x):
-    prime_cheker = set()
+    import numpy as np
+    prime_cheker = np.array([])
     for i in range(2, x//2):
         for j in range(i*i, x//2, i):
-            prime_cheker.add(j)
-        if x % prime_cheker[i] == 0:
+            np.append(prime_cheker,j)
+        if x % prime_cheker == 0:
             print("Not Prime")
             break
-
-k = int(input("Big number give now "))
-sieve_implimented(k)
+sieve_implimented(50)
